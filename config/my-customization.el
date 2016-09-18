c8933594421c6a5e45f4754874b31aebe094d345
DoMiNela10 <retrogamingdomi@wp.pl>
2016-09-18 18:20:06 +0200

    fixed a comment

--- config/my-customization.el
+++ config/my-customization.el
@@ -4,7 +4,7 @@
 ;; use C-h like readlne
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "M-h") 'backward-kill-word)
-(global-set-key (kbd "C-?") 'help-command) ;new binding for help prefix
+(global-set-key (kbd "C-?") 'help-command) ; new binding for help prefix
 ;; Use C-x C-h for marking paragraphs
 (global-set-key (kbd "C-x C-h") 'mark-paragraph)
 ;; other keybindings
