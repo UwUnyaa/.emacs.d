c693e31c12b25b1e3dee5b869fb3100f64a0a87e
DoMiNela10 <retrogamingdomi@wp.pl>
2017-01-31 19:32:19 +0100

    customized dired-dwim-target

--- config/my-customization.el
+++ config/my-customization.el
@@ -78,6 +78,8 @@
   "Alist of org export formats and functions used by them. Used
 by `my-dired-do-org-export'.")
 
+(setq dired-dwim-target t)
+
 (add-hook 'dired-mode-hook
           (lambda ()
             (define-key dired-mode-map "E" 'my-dired-do-org-export)
