edeb59786e30649086d33cdec90daee2538be694
DoMiNela10 <retrogamingdomi@wp.pl>
2017-02-19 21:23:57 +0100

    updated web-mode

--- lisp/web-mode.el
+++ lisp/web-mode.el
@@ -3,7 +3,7 @@
 
 ;; Copyright 2011-2017 François-Xavier Bois
 
-;; Version: 14.0.43
+;; Version: 14.0.44
 ;; Author: François-Xavier Bois <fxbois AT Google Mail Service>
 ;; Maintainer: François-Xavier Bois
 ;; URL: http://web-mode.org
@@ -21,7 +21,7 @@
 
 ;;---- CONSTS ------------------------------------------------------------------
 
-(defconst web-mode-version "14.0.43"
+(defconst web-mode-version "14.0.44"
   "Web Mode version.")
 
 ;;---- GROUPS ------------------------------------------------------------------
@@ -9151,6 +9151,13 @@ or nil if it's empty or it doesn't exist."
          (when (web-mode-block-match)
            (setq end-inside (point))
            (setq end-outside (1+ (web-mode-block-end-position (point)))))
+         )
+        ;; *** html comment folding
+        ((eq (get-text-property (point) 'tag-type) 'comment)
+         (setq beg-outside (web-mode-tag-beginning-position))
+         (setq beg-inside (+ beg-outside 4))
+         (setq end-outside (web-mode-tag-end-position))
+         (setq end-inside (- end-outside 3))
          )
         ;; *** tag folding
         ((or (member (get-text-property (point) 'tag-type) '(start end))
