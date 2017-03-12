95fbe425958a3a5ab9f68a956f376a5bbaf40ca3
DoMiNela10 <retrogamingdomi@wp.pl>
2017-03-12 19:55:32 +0100

    change auto-mode-alist additions for web-mode into a single mapc iteration

--- config/my-extensions.el
+++ config/my-extensions.el
@@ -15,17 +15,13 @@
 ;;; `web-mode'
 
 ;; select mode on filetype
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.twig\\'" . web-mode))
+(mapc
+ (lambda (extension)
+   (add-to-list 'auto-mode-alist
+                (cons (format "\\.%s\\'" extension)
+                      'web-mode)))
+ '("phtml" "tpl\\.php" "[agj]sp" "as[cp]x" "erb" "mustache" "djhtml" "html?"
+   "css" "php" "twig"))
 
 ;; customize variables
 (setq web-mode-enable-auto-pairing t               ; auto-pairing
