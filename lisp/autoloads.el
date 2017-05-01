ad63e2cea9dd811f24176543f4e55ddff05222e6
DoMiNela10 <retrogamingdomi@wp.pl>
2017-05-01 22:17:40 +0200

    udpated autoloads

--- lisp/autoloads.el
+++ lisp/autoloads.el
@@ -114,8 +114,8 @@ Start the web server with given `directory' as `httpd-root'.
 
 ;;;***
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (22791 18873 48776
-;;;;;;  849000))
+;;;### (autoloads nil "web-mode" "web-mode.el" (22791 38882 975406
+;;;;;;  442000))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\
