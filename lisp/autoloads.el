8aa2f8d8b437ea20899eefba779b24f9d6ac90ab
DoMiNeLa10 <retrogamingdomi@wp.pl>
2018-02-06 13:40:16 +0100

    update web-mode

--- lisp/autoloads.el
+++ lisp/autoloads.el
@@ -151,8 +151,8 @@ Start the web server with given `directory' as `httpd-root'.
 
 ;;;***
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (23159 4555 434456
-;;;;;;  242000))
+;;;### (autoloads nil "web-mode" "web-mode.el" (23161 41494 994206
+;;;;;;  906000))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\
