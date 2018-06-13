bbb80ac4a93e9a7f79ce51897eae61c8ee568bb1
DoMiNeLa10 <retrogamingdomi@wp.pl>
2018-06-13 09:07:10 +0200

    add rjsx-mode

---
+++ lisp/rjsx/autoloads.el
@@ -0,0 +1,27 @@
+;;; autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "rjsx-mode" "rjsx-mode.el" (0 0 0 0))
+;;; Generated autoloads from rjsx-mode.el
+
+(autoload 'rjsx-mode "rjsx-mode" "\
+Major mode for editing JSX files.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "rjsx-mode" '("rjsx-")))
+
+;;;***
+
+(provide 'autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; autoloads.el ends here
