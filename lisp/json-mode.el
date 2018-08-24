f0d014c6463eba6207fb01862f6a26b7ce2e8fab
DoMiNeLa10 <retrogamingdomi@wp.pl>
2018-08-24 23:09:45 +0200

    update json-mode

--- lisp/json-mode.el
+++ lisp/json-mode.el
@@ -87,7 +87,7 @@
 ;;;###autoload
 (define-derived-mode json-mode js-mode json-mode-mode-name
   "A simple mode for JSON editing."
-  (when json-mode-pretty-print-on-open
+  (when (and json-mode-pretty-print-on-open (json-mode-buffer-valid-p))
     (json-mode-pretty-print-buffer)
     (goto-char (point-min))    ; this line is ignored in pretty print function
     (set-buffer-modified-p nil))
