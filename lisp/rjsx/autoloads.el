2deb1dcf914b974363980fb7fde96b60158e2ea1
DoMiNeLa10 <retrogamingdomi@wp.pl>
2018-09-11 17:26:30 +0200

    update rjsx-mode

--- lisp/rjsx/autoloads.el
+++ lisp/rjsx/autoloads.el
@@ -11,7 +11,26 @@ Major mode for editing JSX files.
 
 \(fn)" t nil)
 
+(autoload 'rjsx-minor-mode "rjsx-mode" "\
+Minor mode for parsing JSX syntax into an AST.
+
+\(fn &optional ARG)" t nil)
+
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
+
+(autoload 'rjsx-comment-dwim "rjsx-mode" "\
+RJSX implementation of `comment-dwim'. If called on a region,
+this function delegates to `comment-or-uncomment-region'. If the
+point is not in a JSX context, it delegates to the
+`comment-dwim', otherwise it will comment the JSX AST node at
+point using the apppriate comment delimiters.
+
+For example: If point is on a JSX attribute or JSX expression, it
+will comment the entire attribute using \"/* */\". , otherwise if
+it's on a descendent JSX Element, it will use \"{/* */}\"
+instead.
+
+\(fn ARG)" t nil)
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "rjsx-mode" '("rjsx-")))
 
