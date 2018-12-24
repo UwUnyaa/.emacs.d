0a45aafc2c7e4cf47b3dc1d8e5f26ba5b15e358b
DoMiNeLa10 <retrogamingdomi@wp.pl>
2018-12-24 16:17:02 +0100

    move web-mode related stuff to a separate directory

--- lisp/autoloads.el
+++ lisp/autoloads.el
@@ -241,36 +241,6 @@ Start the web server with given `directory' as `httpd-root'.
 
 ;;;***
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (0 0 0 0))
-;;; Generated autoloads from web-mode.el
-
-(autoload 'web-mode "web-mode" "\
-Major mode for editing web templates.
-
-\(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "web-mode" '("web-mode-")))
-
-;;;***
-
-;;;### (autoloads nil "web-mode-plus" "web-mode-plus.el" (0 0 0 0))
-;;; Generated autoloads from web-mode-plus.el
-
-(autoload 'web-mode-plus-bind-keys "web-mode-plus" "\
-Set default keybindings for `web-mode-plus'.
-This function should be run after `web-mode' is loaded.
-
-\(fn)" t nil)
-
-(autoload 'web-mode-plus-set-html-snippets "web-mode-plus" "\
-Replace HTML snippets with ones optimized for `web-mode-plus'.
-
-\(fn)" nil nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "web-mode-plus" '("web-mode-plus-")))
-
-;;;***
-
 ;;;### (autoloads nil "yaml-mode" "yaml-mode.el" (0 0 0 0))
 ;;; Generated autoloads from yaml-mode.el
 
