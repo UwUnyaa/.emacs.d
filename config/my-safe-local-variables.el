b7a4624df6515ebd36ce65fceb9c9f104492c529
UwUnyaa <uwunyaa@outlook.com>
2022-05-10 10:15:21 +0200

    add project-vc-merge-submodules as a safe variable

--- config/my-safe-local-variables.el
+++ config/my-safe-local-variables.el
@@ -1,5 +1,6 @@
 (setq safe-local-variable-values
-      '((js2-include-browser-externs . t) ; js2
+      '((project-vc-merge-submodules)
+        (js2-include-browser-externs . t) ; js2
         (js2-include-jslint-globals . t)
         (js2-include-node-externs . t)
         (js2-include-rhino-externs . t)))
